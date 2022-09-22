404fa9bd70c67abc283c71e7e784a7798e36e14c
MarekSkrobarczyk <114141740+MarekSkrobarczyk@users.noreply.github.com>
2022-09-22 10:25:06 +0200

    Create Questions.h header file

---
+++ Questions.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+//Header files hold functions andclass definitions 
+
+// Function prototypes 
+
+int questions(int x, int y, std::string name);
