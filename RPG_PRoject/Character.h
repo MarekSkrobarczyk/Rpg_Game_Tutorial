9a20a8aad907a09daf2b1e57e87c78daa28a2c77
Marek Skrobarczyk <marekskrobarczyk@gmail.com>
2022-10-07 15:00:38 +0200

    declare Get functions for stats

--- RPG_PRoject/Character.h
+++ RPG_PRoject/Character.h
@@ -11,5 +11,13 @@ public:
 
 	void setupStats(int _hp, int _str, int _def, int _agi);
 	void displayStats();
+
+	int getHP();
+	int getHPmax();
+	int getAGI();
+
+	int getDamaged();
+	void takeDamage(int damgage)
+
 };
 
